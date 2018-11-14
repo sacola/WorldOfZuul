ebb3b5d71310b4b1af5e1e9c5004eea652e77da2
Bruno Henrique De Mello Junker <37717467+sacola@users.noreply.github.com>
2018-11-14 19:34:06 -0200

    atualização de codigo

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+
 /**
  *  Esta é a classe principal do jogo World of Zuul. 
  *  
@@ -22,7 +23,7 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theatre, pub, lab, office;
+        Room outside, theatre, pub, lab, office, attic;
       
         // create the rooms
         outside = new Room("fora da entrada principal da universidade");
@@ -30,13 +31,19 @@ public class Game
         pub = new Room("na cantina do campus");
         lab = new Room("em um laboratório de informática");
         office = new Room("na sala dos professores");
+        attic = new Room("no sótão da laboratório");
         
         // initialise room exits
-        outside.setExits(null, theatre, lab, pub);
-        theatre.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        outside.setExits("leste", theatre);
+        outside.setExits("sul", lab);
+        outside.setExits("oeste", pub);
+        theatre.setExits("oeste", outside);
+        pub.setExits("leste", outside);
+        lab.setExits("norte", outside);
+        lab.setExits("leste", office);
+        lab.setExits("cima", attic);
+        attic.setExits("baixo", lab);
+        office.setExits("oeste", lab);
 
         currentRoom = outside;  // Começa o jogo fora 
     }
@@ -69,21 +76,15 @@ public class Game
         System.out.println("Mundo de Zuul é um jogo de aventura, incrivelmente chato.");
         System.out.println("Digite 'ajuda' se você precisar de ajuda.");
         System.out.println();
-        System.out.println("Você está " + currentRoom.getDescription());
-        System.out.print("Saídas: ");
-        if(currentRoom.northExit != null) {
-            System.out.print("norte ");
-        }
-        if(currentRoom.eastExit != null) {
-            System.out.print("leste ");
-        }
-        if(currentRoom.southExit != null) {
-            System.out.print("sul ");
-        }
-        if(currentRoom.westExit != null) {
-            System.out.print("oeste ");
-        }
-        System.out.println();
+        printLocationInfo();
+    }
+    
+    /**
+     * Imptimr informações relativas à
+     * Localização atual.
+     */
+    private void printLocationInfo() {
+        System.out.println(currentRoom.getLongDescription());
     }
 
     /**
@@ -142,40 +143,14 @@ public class Game
         String direction = command.getSecondWord();
 
         // Try to leave current room.
-        Room nextRoom = null;
-        if(direction.equals("norte")) {
-            nextRoom = currentRoom.northExit;
-        }
-        if(direction.equals("leste")) {
-            nextRoom = currentRoom.eastExit;
-        }
-        if(direction.equals("sul")) {
-            nextRoom = currentRoom.southExit;
-        }
-        if(direction.equals("oeste")) {
-            nextRoom = currentRoom.westExit;
-        }
+        Room nextRoom = currentRoom.getExit(direction);
 
         if (nextRoom == null) {
             System.out.println("Não há uma porta!");
         }
         else {
             currentRoom = nextRoom;
-            System.out.println("Você está " + currentRoom.getDescription());
-            System.out.print("Saídas: ");
-            if(currentRoom.northExit != null) {
-                System.out.print("norte ");
-            }
-            if(currentRoom.eastExit != null) {
-                System.out.print("leste ");
-            }
-            if(currentRoom.southExit != null) {
-                System.out.print("sul ");
-            }
-            if(currentRoom.westExit != null) {
-                System.out.print("oeste ");
-            }
-            System.out.println();
+            printLocationInfo();
         }
     }
 
